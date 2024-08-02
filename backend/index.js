4a8a3bf06524ed38949a4982e7bd9f3ac3a0b9f4
matthew Wong <matthew.wong128@gmail.com>
2024-08-02 16:50:43 -0400

    removed notes[] and created new dist buid;

--- backend/index.js
+++ backend/index.js
@@ -3,31 +3,9 @@ const Person = require('./model/personDB');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const PORT = process.env.PORT;
-//
 const app = express();
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+const PORT = process.env.PORT;
+
 morgan.token('person', (req, res) => {
   if (req.method == 'GET') return '';
   return JSON.stringify(req.body);
@@ -106,7 +84,6 @@ app.post('/api/persons', (req, res) => {
       const personToAdd = new Person({
         ...person,
       });
-      persons = persons.concat(personToAdd);
       personToAdd.save().then(() => console.log('added person succesfully '));
       res.status(201).json(personToAdd);
     })
